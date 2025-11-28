63d0b065c4d1781d496a616b4c9d01e1f4305c33
Septian Adi Pratama <septianadip7998@gmail.com>
2025-11-28 17:02:59 +0700

    add comments: every single endpoint

--- src/modules/course/course.controller.ts
+++ src/modules/course/course.controller.ts
@@ -17,26 +17,31 @@ import { UpdateCourseDto } from './course.model';
 export class CourseController {
   constructor(private courseService: CourseService) {}
 
+  // endpoint get all courses
   @Get()
   async findAllCourses() {
     return await this.courseService.findAllCourses();
   }
 
+  // endpoint add a course
   @Post()
   async createCourse(@Body() payload: CreateCourseDto) {
     return await this.courseService.createCourse(payload);
   }
 
+  // endpoint delete a course
   @Delete(':id')
   async deleteCourse(@Param('id') id: string) {
     return await this.courseService.deleteCourse(id);
   }
 
+  // endpoint edit a course
   @Put(':id')
   async updateCourse(@Param('id') id: string, @Body() payload: UpdateCourseDto) {
     return await this.courseService.updateCourse(id, payload)
   }
 
+  // endpoint get a course
   @Get(':id')
   async getCourseById(@Param('id') id: string){
     return await this.courseService.getCourseById(id)
