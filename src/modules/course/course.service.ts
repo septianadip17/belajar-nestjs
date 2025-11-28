96f8be6bd4457b7ff818bc6212d681aa1e1b361b
Septian Adi Pratama <septianadip7998@gmail.com>
2025-11-28 17:03:59 +0700

    add comments: every single func

--- src/modules/course/course.service.ts
+++ src/modules/course/course.service.ts
@@ -7,22 +7,27 @@ import { CreateCourseDto, UpdateCourseDto } from "./course.model";
 export class CourseService {
     constructor(private courseRepository: CourseRepository) {}
 
+    // get all courses
     async findAllCourses() {
         return this.courseRepository.findAllCourses();
     }
 
+    // add a course
     async createCourse(payload: CreateCourseDto) {
         return this.courseRepository.createCourse(payload);
     }
 
+    // delete a course
     async deleteCourse(id: string){
         return this.courseRepository.deleteCourse(id)
     }
 
+    // edit a course
     async updateCourse(id: string, payload: UpdateCourseDto){
         return this.courseRepository.updateCourse(id, payload)
     }
 
+    // get a course
     async getCourseById(id: string){
         return this.courseRepository.getCourseById(id)
     }
