871ed2d17639759705366f5ecd0c138db2bafd65
Septian Adi Pratama <septianadip7998@gmail.com>
2025-11-28 17:01:39 +0700

    add comments: every single endpoint

--- src/modules/student/student.controller.ts
+++ src/modules/student/student.controller.ts
@@ -7,28 +7,33 @@ import { AddStudentDto, EditStudentDto } from './student.model';
 export class StudentController {
   constructor(private studentService: StudentService) { }
 
+  // endpoint get all students
   @Get()
   async findAllStudents() {
     return await this.studentService.findAllStudents();
   }
 
+  // endpoint get all students
   @Post()
   async addStudent(@Body() payload: AddStudentDto) {
     console.log(payload.courseId)
     return await this.studentService.addStudent(payload);
   }
 
+  // endpoint delete a student
   @Delete(':id')
   async deleteStudent(@Param('id') id: string) {
     console.log(id)
     return await this.studentService.deleteStudent(id)
   }
 
+  // endpoint edit a student
   @Put(':id')
   async editStudent(@Param('id') id: string, @Body() payload: EditStudentDto) {
     return await this.studentService.editStudent(id, payload)
   }
 
+  // endpoint get a student
   @Get(':id')
   async getStudentById(@Param('id') id: string){
     return await this.studentService.getStudentById(id)
